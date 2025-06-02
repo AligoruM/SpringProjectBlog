634c2d1c91d1fecc2332244a4aaf83572be8b71f
Sergei <gentor010101@gmail.com>
2025-06-02 07:47:53 +0300

    fixed issue with images

--- src/main/java/org/practice/config/WebConfiguration.java
+++ src/main/java/org/practice/config/WebConfiguration.java
@@ -1,22 +1,38 @@
 package org.practice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"org.practice"})
 @PropertySource("classpath:application.properties")
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private Environment env;
+
+    @Autowired
+    public WebConfiguration(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + env.getProperty("image.location"));
+    }
 }
