eee8c6acce75910b8a4f6aad2762c35f0cbe3119
Sergey <gentor010101@gmail.com>
2025-06-13 11:56:03 +0300

    added junit test with assertJ, I like it more that junit or hamster assertion style

--- src/main/java/org/practice/utils/TagNormalizer.java
+++ src/main/java/org/practice/utils/TagNormalizer.java
@@ -14,6 +14,7 @@ public class TagNormalizer {
                 .map(String::trim)
                 .map(s -> StringUtils.stripStart(s, "#"))
                 .map(StringUtils::uncapitalize)
+                .distinct()
                 .toList();
     }
 }
---
+++ src/test/java/org/practice/utils/TagNormalizerTest.java
@@ -0,0 +1,57 @@
+package org.practice.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TagNormalizerTest {
+
+    @Test
+    public void testTagNormalizer_validTag_shouldReturnSameString() {
+        String input = "tag1";
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized.getFirst()).isEqualTo("tag1");
+    }
+
+    @Test
+    public void testTagNormalizer_emptyTag_shouldReturnEmptyCollection() {
+        String input = "";
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized).hasSize(0);
+    }
+
+    @Test
+    public void testTagNormalizer_2tags_shouldReturn2tags() {
+        String input = "tag1 tag2";
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized).hasSize(2);
+        assertThat(normalized).containsOnly("tag1", "tag2");
+    }
+
+    @Test
+    public void testTagNormalizer_capitalizedTag_shouldReturnUncapitalized() {
+        String input = "Tag1";
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized).hasSize(1);
+        assertThat(normalized.getFirst()).isEqualTo("tag1");
+    }
+
+    @Test
+    public void testTagNormalizer_2sameTags_shouldReturn1tag() {
+        String input = "tag1 tag1";
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized).hasSize(1);
+        assertThat(normalized).containsOnly("tag1");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "\n"})
+    public void testTagNormalizer_spaceSymbolsStringTag_shouldReturnEmptyCollection(String input) {
+        List<String> normalized = TagNormalizer.normalize(input);
+        assertThat(normalized).hasSize(0);
+    }
+}
